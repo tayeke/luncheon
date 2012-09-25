f2cdd8889b9a7c2375316aa507f015cd4d8bb129
Taylor j. Eke <tayloreke@gmail.com>
2012-09-24 19:46:47 -0700

    fixed redis config

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,4 +1,4 @@
-if RAILS_ENV == 'production'
+if Rails.env.production?
   uri = URI.parse(ENV["REDISTOGO_URL"])
   REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 else
